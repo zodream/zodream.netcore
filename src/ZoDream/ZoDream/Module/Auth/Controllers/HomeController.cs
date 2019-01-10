7a159958538a7912b4b55d2eae9fb8ca9de47142
ZoDream <648383079@qq.com>
2019-01-10 23:09:01 +0800

    上传

---
+++ src/ZoDream/ZoDream/Module/Auth/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ZoDream.Module.Auth.Controllers
+{
+    [Area("Auth")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
